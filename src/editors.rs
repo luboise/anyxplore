08de2a5d8aadbe10dc59e56f06d2403c5954be49
luboise <lucas2@live.com.au>
2025-09-03 11:55:39 +1000

    Refactored viewer and editor systems

---
+++ src/editors.rs
@@ -0,0 +1,38 @@
+use bnl::asset::texture::Texture;
+use fltk::{frame, image::RgbImage, prelude::*};
+
+pub trait Viewable {
+    fn create_viewer<T: GroupExt>(&self, widget_parent: &mut T);
+}
+
+pub trait Editable: Viewable {
+    fn create_editor<T: GroupExt>(&mut self, widget_parent: &mut T);
+}
+
+impl Viewable for Texture {
+    fn create_viewer<T: GroupExt>(&self, widget_parent: &mut T) {
+        if let Ok(rgba) = self.to_rgba_image() {
+            if let Ok(img) = unsafe {
+                RgbImage::from_data(
+                    rgba.bytes(),
+                    rgba.width() as i32,
+                    rgba.height() as i32,
+                    fltk::enums::ColorDepth::Rgba8,
+                )
+            } {
+                println!("Setting image.");
+
+                let mut frame = frame::Frame::default().with_size(img.width(), img.height());
+                frame.set_image(Some(img));
+
+                widget_parent.add(&frame);
+            }
+        }
+    }
+}
+
+impl Editable for Texture {
+    fn create_editor<T: GroupExt>(&mut self, widget_parent: &mut T) {
+        todo!();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
-    env, fs,
+    env,
+    fmt::Display,
+    fs,
     path::{Path, PathBuf},
 };
 
@@ -14,10 +16,13 @@ use fltk::{
     group::{Flex, Scroll},
     image::RgbImage,
     prelude::*,
-    tree::{self},
+    tree::{self, TreeItem},
     window,
 };
 
+use crate::editors::{Editable, Viewable};
+
+mod editors;
 mod widgets;
 
 #[derive(Copy, Clone)]
@@ -31,13 +36,69 @@ struct BNLStruct {
     path: PathBuf,
 
     descriptions: Option<Vec<AssetDescription>>,
-    data: BNLFile,
+    bnl_file: BNLFile,
+}
+
+#[derive(Debug)]
+enum XError {
+    AlreadyLoaded,
+    TreeError,
+    NodeError,
+}
+
+impl Display for XError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl BNLStruct {
+    pub fn load_asset_descriptions(&mut self) -> Result<&Vec<AssetDescription>, XError> {
+        println!("Loading asset descriptions for {}", self.path.display());
+
+        let descriptions = self.bnl_file.asset_descriptions().to_vec();
+        self.descriptions = Some(descriptions);
+
+        // Unwrap can't fail since we just assigned it
+        Ok(&self.descriptions.as_ref().unwrap())
+    }
 }
 
 #[derive(Clone)]
 struct NodeData {
     is_root: bool,
     bnl_index: usize,
+}
+
+struct EditWindow {
+    scroll: Scroll,
+}
+
+impl EditWindow {
+    pub fn reset(&mut self) {
+        self.scroll.clear()
+    }
+
+    pub fn add_viewer<V: Viewable>(&mut self, viewable: V) {
+        viewable.create_viewer(&mut self.scroll);
+        self.scroll.scroll_to(0, 0);
+        self.scroll.redraw();
+    }
+
+    pub fn add_editor<E: Editable>(&mut self, editable: &mut E) {
+        editable.create_editor(&mut self.scroll);
+
+        self.scroll.scroll_to(0, 0);
+        self.scroll.redraw();
+    }
+
+    fn scroll_mut(&mut self) -> &mut Scroll {
+        &mut self.scroll
+    }
+
+    fn scroll(&self) -> &Scroll {
+        &self.scroll
+    }
 }
 
 struct AnyXPloreApp {
@@ -49,9 +110,9 @@ struct AnyXPloreApp {
 
     tree: tree::Tree,
 
-    bnl_files: Vec<BNLStruct>,
+    bnl_structs: Vec<BNLStruct>,
 
-    edit_window: Scroll,
+    edit_window: EditWindow,
 
     main_win: window::Window,
 
@@ -107,7 +168,7 @@ impl AnyXPloreApp {
         let bnl_files = vec![BNLStruct {
             tree_id,
             descriptions: None,
-            data: new_bnl,
+            bnl_file: new_bnl,
             path: pb,
         }];
 
@@ -136,12 +197,71 @@ impl AnyXPloreApp {
             app,
             tree,
             main_win,
-            edit_window: scroll,
+            edit_window: EditWindow { scroll },
             count,
             receiver,
-            bnl_files,
+            bnl_structs: bnl_files,
             flex,
             image_frame,
+        }
+    }
+
+    pub fn on_tree_item_clicked(&mut self) -> Result<(), XError> {
+        let node = self.tree.first_selected_item().ok_or(XError::TreeError)?;
+        let node_data: NodeData = unsafe { node.user_data() }.ok_or_else(|| {
+            eprintln!("No node data available.");
+            XError::TreeError
+        })?;
+
+        // If a root node is clicked, ie a BNL file, handle it
+        if node_data.is_root {
+            let bnl_struct = self
+                .bnl_structs
+                .get_mut(node_data.bnl_index)
+                .ok_or(XError::TreeError)?;
+
+            // If the descriptions aren't loaded, load them and put them into the tree
+            if bnl_struct.descriptions.is_none() {
+                bnl_struct
+                    .load_asset_descriptions()?
+                    .iter()
+                    .for_each(|desc| {
+                        let mut new_node = self.tree.insert(&node, desc.name(), i32::MAX).unwrap();
+
+                        new_node.set_user_data(NodeData {
+                            is_root: false,
+                            bnl_index: node_data.bnl_index,
+                        });
+                    });
+            }
+
+            return Ok(());
+        } else {
+            let bnl_struct = self
+                .bnl_structs
+                .get_mut(node_data.bnl_index)
+                .ok_or(XError::NodeError)?;
+
+            let asset_name = node.label().unwrap_or_default();
+
+            match bnl_struct
+                .bnl_file
+                .get_raw_asset(&asset_name)
+                .map_err(|_| XError::NodeError)?
+                .asset_type
+            {
+                AssetType::ResTexture => {
+                    if let Ok(tex) = bnl_struct.bnl_file.get_asset::<Texture>(&asset_name) {
+                        self.edit_window.reset();
+                        self.edit_window.add_viewer(tex);
+                    }
+                }
+                t => {
+                    println!("Type {:?} not implemented yet.", t);
+                }
+            };
+
+            Ok(())
         }
     }
 
@@ -149,87 +269,9 @@ impl AnyXPloreApp {
         while self.app.wait() {
             if let Some(msg) = self.receiver.recv() {
                 match msg {
-                    Message::TreeClicked => {
-                        if let Some(node) = self.tree.first_selected_item() {
-                            let node_data: NodeData = match unsafe { node.user_data() } {
-                                Some(d) => d,
-                                None => {
-                                    eprintln!("No node data available.");
-                                    return;
-                                }
-                            };
-
-                            if node_data.is_root {
-                                if let Some(bnl_struct) =
-                                    self.bnl_files.get_mut(node_data.bnl_index)
-                                {
-                                    if bnl_struct.descriptions.is_none() {
-                                        println!(
-                                            "Loading asset descriptions for {}",
-                                            bnl_struct.path.display()
-                                        );
-
-                                        let descriptions = Vec::new();
-
-                                        bnl_struct.data.asset_descriptions().iter().for_each(
-                                            |desc| {
-                                                let mut new_node = self
-                                                    .tree
-                                                    .insert(&node, desc.name(), i32::MAX)
-                                                    .unwrap();
-
-                                                new_node.set_user_data(NodeData {
-                                                    is_root: false,
-                                                    bnl_index: node_data.bnl_index,
-                                                });
-                                            },
-                                        );
-
-                                        bnl_struct.descriptions = Some(descriptions);
-                                    }
-                                }
-                            } else if let Some(bnl_struct) =
-                                self.bnl_files.get_mut(node_data.bnl_index)
-                            {
-                                let asset_name = node.label().unwrap_or_default();
-
-                                if let Ok(raw) = bnl_struct.data.get_raw_asset(&asset_name) {
-                                    if raw.asset_type == AssetType::ResTexture {
-                                        if let Ok(tex) =
-                                            bnl_struct.data.get_asset::<Texture>(&asset_name)
-                                        {
-                                            if let Ok(rgba) = tex.to_rgba_image() {
-                                                if let Ok(img) = unsafe {
-                                                    RgbImage::from_data(
-                                                        rgba.bytes(),
-                                                        rgba.width() as i32,
-                                                        rgba.height() as i32,
-                                                        fltk::enums::ColorDepth::Rgba8,
-                                                    )
-                                                } {
-                                                    println!("Setting image.");
-
-                                                    self.image_frame.resize(
-                                                        self.image_frame.x(),
-                                                        self.image_frame.y(),
-                                                        img.width(),
-                                                        img.height(),
-                                                    );
-
-                                                    // self.image_frame.set_image::<RgbImage>(None);
-                                                    // self.image_frame.redraw();
-
-                                                    self.image_frame.set_image(Some(img));
-
-                                                    self.image_frame.parent().unwrap().redraw();
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    Message::TreeClicked => self.on_tree_item_clicked().unwrap_or_else(|e| {
+                        eprintln!("Unable to handle tree click.\nError: {}", e);
+                    }),
                 }
             }
         }
